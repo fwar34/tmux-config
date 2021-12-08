c52e54718b6bf66f5edd5a0054eb2e3492644c24
fwar34 <fwar34@126.com>
2021-12-08 22:11:54 +0800

    update

---
+++ my_adjust_for_output.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/lua
+
+-- 1447/7919MB [          ]  2.0% 0.32 0.73 0.60
+-- %d 数字 %D 非数字 %:转义特殊字符 %s
+io.input(io.stdin)
+local out = io.read()
+-- local out = "2065/7919MB [||        ] 28.5% 0.46 0.60 0.59"
+local mem, cpu = out:match("(%d*MB)[%s%[%]|]*(%d+%.%d%%)")
+print(mem, cpu)
