e085110b7716fe3c05d04d41ac8b611270e2ec5f
fwar34 <fwar34@126.com>
2022-04-20 20:22:29 +0800

    update

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ ln -sf $HOME/mine/tmux-config/.tmux.mine.conf $HOME/.tmux.conf
 
 # cd ~/.tmux && git submodule init && git submodule update
 
-cd ~/mine/tmux-config/vendor/tmux-mem-cpu-load && cmake . && make && sudo make install
-cd ~/mine/tmux-config/collect-go && sudo make install
+#cd ~/mine/tmux-config/vendor/tmux-mem-cpu-load && cmake . && make && sudo make install
+cd ~/mine/tmux-config/collect-go && make install
 
 tmux source-file ~/.tmux.conf
