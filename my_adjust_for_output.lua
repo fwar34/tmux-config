d33bc4923256bc067a8a75ac2b730aa956c6ede6
fwar34 <fwar34@126.com>
2022-08-08 21:05:25 +0800

    update

--- my_adjust_for_output.lua
+++ my_adjust_for_output.lua
@@ -5,4 +5,20 @@ io.input(io.stdin)
 local out = io.read()
 -- local out = "2065/7919MB [||        ] 28.5% 0.46 0.60 0.59"
 local mem, cpu = out:match("(.*MB)[%s%[%]|]*(%d+%.%d%%)")
-print(mem, " ", cpu)
+-- print(mem, " ", cpu)
+
+local temp = function ()
+    if os.execute('sensors > /dev/null 2>&1') then
+        local t = io.popen('sensors')
+        local output = t:read('*all')
+        local temp = output:match("Package id 0:%s*+(%d*%.*%d*).*")
+        return temp .. ' °C'
+    end
+end
+
+local core_temp = temp()
+if core_temp then
+    print(core_temp, ' | ', mem, ' | ', cpu)
+else
+    print(mem, ' | ', cpu)
+end
